8f48ba1390404cff5f7f4063ec0b93acdcf4788d
abbosturaboev <112198647+abbosturaboev@users.noreply.github.com>
2022-09-11 20:57:18 +0300

    Самое большое число

---
+++ задача/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[new Random().Next(1,1000)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-1000,1000);
+}
+int max = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.WriteLine(array[i] + " ");
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+}
+System.Console.WriteLine("Самое большое число = " + max);
